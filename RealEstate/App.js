ca48af877605d71e9b38404f5130a5f5bf0da2a9
JacquelineFM <jfloresm.dev@gmail.com>
2021-04-08 16:08:35 -0500

    Added and styles and fonts.
    Styles were added to the real estate list components, fonts were
    installed for the titles and subtitles in the list, and
    added to the styles.

--- RealEstate/App.js
+++ RealEstate/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
-import Navigator from './Navigators/Navigator';
+import Navigator from './src/Navigators/Navigator';
 
 export default function App() {
   return (
